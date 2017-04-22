b05c21b9070ce45f531650acc09d196b291d3cb8
Brandon White <bmwqg@live.com>
2017-04-22 17:30:54 -0400

    updated with stock info script; added scaffold for stock

--- StockApp/stock.js
+++ StockApp/stock.js
@@ -1,1 +1,38 @@
-console.log("I'm alive");
+$(document).ready(function(){
+
+	$.get({
+		data: {
+			input: "AAPL"
+		},
+		dataType: "jsonp",
+		url: "http://dev.markitondemand.com/Api/v2/Lookup/jsonp",
+		complete: function(data){
+			console.log(data);
+			response = data.responseJSON;
+
+			// symbol info
+			var symbol = response.Symbol;
+			var name = response.Name;
+
+			// price info
+			var low = response.Low;
+			var high = response.High;
+			var open = response.Open;
+			var lastPrice = response.LastPrice; //used
+			var timestamp = response.Timestamp; //used
+
+			// change info
+			var change = response.Change; //used
+			var changePercent = response.ChangePercent; //used
+
+			// YTD info
+			var changeYTD = response.ChangeYTD;
+			var changePercentYTD = response.ChangePercentYTD //used
+
+			// stock qty info
+			var marketCap = response.MarketCap;
+			var volume = response.Volume;
+		}
+	});
+
+});
