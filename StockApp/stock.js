6a659cfce641fc36ac1fa08a84da3f9abe85b07e
Michael Frederick <michaelfrederick167@gmail.com>
2017-04-22 16:49:50 -0400

    Setup event Handlers

--- StockApp/stock.js
+++ StockApp/stock.js
@@ -1,1 +1,19 @@
-console.log("I'm alive");
+$(window).ready(function() {
+	bindEventHandlers();
+});
+
+function bindEventHandlers() {
+	$("#searchBar").keyup(function(e) {
+		if (e.keyCode == 13) {
+			doSearch();
+		}
+	});
+
+	$("#searchButton").click(function() {
+		doSearch();
+	});
+};
+
+function doSearch() {
+	console.log("searching");
+}
